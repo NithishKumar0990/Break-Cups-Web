0d6fa60afc1997ee604c2f4668b480e6364ecc5f
Nithish Kumar Lakshmana Perumal <nithishkumarl168@gmail.com>
2026-06-11 00:37:17 +0530

    Initial commit

--- backend/server.js
+++ backend/server.js
@@ -11,9 +11,9 @@ app.use(express.json());
 
 
 const db = mysql.createConnection({
-    host: 'localhost',
+    host: '127.0.0.1',
     user: 'root',
-    password: 'root', 
+    password: '', 
     database: 'break_cup_db'
     });
 
